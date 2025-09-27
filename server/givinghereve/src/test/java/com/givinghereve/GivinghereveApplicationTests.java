49428ebac2e4f3d9b01bbc970722f67efae2a46c
shamos13 <akwatuha04@gmail.com>
2025-09-27 09:57:12 +0300

    Added the Backend Structure

---
+++ server/givinghereve/src/test/java/com/givinghereve/GivinghereveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.givinghereve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GivinghereveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
